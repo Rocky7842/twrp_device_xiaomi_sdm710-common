572d73941e530fd5c14d886e4e8327505525e91e
Albert I <kras@raphielgang.org>
2019-08-02 03:48:14 +0800

    sdm710-common: Set custom CPU temperature path
    * Points to 1st silver core, which is what we want.
    * Fixes absurd CPU temperature glitch shown on TWRP.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 # TWRP
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone9/temp
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
