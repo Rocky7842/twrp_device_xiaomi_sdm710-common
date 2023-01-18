80f6d18dc5611454053faade2be8f1bfaaf9c325
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-18 16:37:04 +0800

    sdm710-common: Stop allowing to opt-out decryption support
    Since userdata entry has been added to the common fstab, it makes no sense
    Also remove `MI710_USES_DEVICE_SPECIFIC_FSTAB` for the same reason

--- sdm710.mk
+++ sdm710.mk
@@ -14,14 +14,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Crypto
-ifeq ($(MI710_INCLUDE_CRYPTO_FBE),true)
-MI710_INCLUDE_CRYPTO := true
-PRODUCT_PACKAGES += qcom_decrypt_fbe
-endif
-ifeq ($(MI710_INCLUDE_CRYPTO),true)
-PRODUCT_PACKAGES += qcom_decrypt
+PRODUCT_PACKAGES += \
+    qcom_decrypt_fbe \
+    qcom_decrypt
+
 MI710_KEYMASTER_VERSION ?= 4.0
-endif # MI710_INCLUDE_CRYPTO
 
 # Debug
 PRODUCT_PACKAGES += \
@@ -33,29 +30,20 @@ PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/system/apex/com.android.runtime/bin/crash_dump64:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/crash_dump64
 
 # Fstab
-ifneq ($(MI710_USES_DEVICE_SPECIFIC_FSTAB),true)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/fstab/,$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/)
-endif
 
 # Proprietary - BEGIN
 ifneq ($(MI710_USES_DEVICE_SPECIFIC_BLOBS),true)
 
 # Proprietary - QSEECOMd
-ifeq ($(MI710_INCLUDE_CRYPTO),true)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/qseecomd/system/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
-endif
 
 # Proprietary - QTI Gatekeeper 1.0
-ifeq ($(MI710_INCLUDE_CRYPTO),true)
-ifeq ($(MI710_INCLUDE_CRYPTO_FBE),true)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/qti-gatekeeper-1-0/system/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
-endif
-endif
 
 # Proprietary - QTI Keymaster
-ifeq ($(MI710_INCLUDE_CRYPTO),true)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/qti-keymaster-common/system/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
 ifeq ($(MI710_KEYMASTER_VERSION),3.0)
@@ -65,18 +53,15 @@ else ifeq ($(MI710_KEYMASTER_VERSION),4.0)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/qti-keymaster-4-0/system/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
 endif
-endif
 
 # Proprietary - END
 endif # MI710_USES_DEVICE_SPECIFIC_BLOBS
 
 # Vintf - Keymaster
-ifeq ($(MI710_INCLUDE_CRYPTO),true)
 ifeq ($(MI710_KEYMASTER_VERSION),3.0)
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/vintf/keymaster-3-0.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/etc/vintf/manifest/keymaster-3-0.xml
 else ifeq ($(MI710_KEYMASTER_VERSION),4.0)
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/vintf/keymaster-4-0.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/etc/vintf/manifest/keymaster-4-0.xml
-endif
 endif
 
 # Soong namespaces
