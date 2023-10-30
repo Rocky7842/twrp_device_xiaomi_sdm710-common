ca5d600f5b2fef118f8412e8825feef94d7f023d
Rocky7842 <eric.rocky7842@gmail.com>
2023-10-30 14:05:08 +0800

    sdm710-common: Correct fstab entries to ro

--- fstab/merge-fstab/bin/merge-fstab.sh
+++ fstab/merge-fstab/bin/merge-fstab.sh
@@ -7,13 +7,13 @@ echo >> /system/etc/twrp.flags
 
 if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep qti_dynamic_partitions > /dev/null; then
     for p in system system_ext product vendor odm; do
-        echo "${p} /${p} ext4 rw,barrier=1,discard wait,logical" >> /system/etc/recovery.fstab
+        echo "${p} /${p} ext4 ro,barrier=1,discard wait,logical" >> /system/etc/recovery.fstab
     done
 else
     for p in vendor cust; do
-        echo "/dev/block/bootdevice/by-name/${p} /${p} ext4 rw,barrier=1 wait,formattable" >> /system/etc/recovery.fstab
+        echo "/dev/block/bootdevice/by-name/${p} /${p} ext4 ro,barrier=1 wait,formattable" >> /system/etc/recovery.fstab
     done
-    echo "/dev/block/bootdevice/by-name/system /system_root ext4 rw,barrier=1 wait,formattable" >> /system/etc/recovery.fstab
+    echo "/dev/block/bootdevice/by-name/system /system_root ext4 ro,barrier=1 wait,formattable" >> /system/etc/recovery.fstab
 
     cat /system/etc/twrp.flags.nondynamic >> /system/etc/twrp.flags
 fi
